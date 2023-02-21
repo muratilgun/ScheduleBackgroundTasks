8fb6a76f920fb1e6c99c0cfa48ac4ec9361711da
muratilgun <muratilgun34@gmail.com>
2023-02-21 22:42:43 +0300

    Added new task

--- ScheduleBackgroundTasks/ScheduleBackgroundTasks/Program.cs
+++ ScheduleBackgroundTasks/ScheduleBackgroundTasks/Program.cs
@@ -8,17 +8,16 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container.
 
         builder.Services.AddControllers();
         builder.Services.AddSingleton<IHostedService,SampleTask1>();
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddSingleton<IHostedService,SampleTask2>();
+
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
 
         var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {
             app.UseSwagger();
---
+++ ScheduleBackgroundTasks/ScheduleBackgroundTasks/ScheduleTask/SampleTask2.cs
@@ -0,0 +1,19 @@
+﻿using ScheduleBackgroundTasks.BackgroundTasks;
+
+namespace ScheduleBackgroundTasks.ScheduleTask;
+public class SampleTask2 : ScheduledProcessor
+{
+    public SampleTask2(IServiceScopeFactory serviceScopeFactory) : base(serviceScopeFactory)
+    {
+    }
+
+    protected override string Schedule => "*/2 * * * *";
+
+    public override async Task ProcessInScope(IServiceProvider scopeServiceProvider)
+    {
+        Random random = new();
+        int randomNumber =  random.Next(1, 100);
+        Console.WriteLine("SampleTask2 : " + DateTime.Now.ToString() + " Random Number : " + randomNumber.ToString());
+        await Task.Run(() => Task.CompletedTask);
+    }
+}
